b7fe6e6e2a9bea0d00765fdda075636f40f971fc
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-11-02 18:43:59 -0300

    testando animações com perspective

--- JS/script.js
+++ JS/script.js
@@ -2,11 +2,26 @@ const card = document.querySelector(".container");
 
 card.addEventListener("mousemove", cardMovimneto);
 
-function cardMovimneto() {
+function cardMovimneto(event) {
     const cardWidth = card.offsetWidth;
     const cardHeight = card.offsetHeight;
-    const centroPagina = card.offsetLeft;
-    document.querySelector("h2").innerHTML = centroPagina;
+    const centroPaginaX = card.offsetLeft + cardWidth / 2;
+    const centroPaginaY = card.offsetTop + cardHeight / 2;
+    const posicaoMouseX = event.clientX - centroPaginaX;
+    const posicaoMouseY = event.clientY - centroPaginaY;
+    
+
+    const rotateX = (25 * posicaoMouseY / (cardHeight / 2)).toFixed(2);
+
+    const rotateY = (25 * posicaoMouseX / (cardWidth / 2)).toFixed(2);
+
+    document.querySelector("h2").innerHTML = rotateX;
+    document.querySelector("h3").innerHTML = rotateY;
+
+    card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+   
+   
+    
     
     
 }
--- Readme.md
+++ Readme.md
@@ -21,4 +21,4 @@
 - vamos pegar o centro do cartão, mas referente a página toda, da mesma forma com o metodo
 - <img width="257" alt="image" src="https://github.com/luizneto375/Cart-o-Nubank/assets/114784434/31a8a0dc-6d76-4bbe-aed0-190856bb91d9">
 
-
+OBS: pegamos essas posições para podermos fazer a nossa animação que vai ser, basicamente feito em torno do eixo central 
