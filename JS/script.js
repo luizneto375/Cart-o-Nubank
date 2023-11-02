60eb2fbef6c64e69b7728458ae0770e7c4567626
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-11-02 17:24:32 -0300

    passando metodo, para pegar a largura do card

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,8 @@
 const card = document.querySelector(".container");
 
-card.addEventListener("mousemove", )
+card.addEventListener("mousemove", cardMovimneto);
+
+function cardMovimneto() {
+    const cardWidth = card.offsetWidth;
+    console.log(cardWidth);
+}
