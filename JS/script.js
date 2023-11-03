82887737c6ff9e453530fbe1814c2d7dc964c5bf
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-11-02 22:31:46 -0300

    finalizando efeitos

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,10 @@
 const card = document.querySelector(".container");
 
-card.addEventListener("mouseout", cardMovimneto);
+card.addEventListener("mousemove", cardMovimneto);
+
+card.addEventListener("mouseleave", cardBack);
+
+card.addEventListener("mouseenter", cardEnter); 
 
 function cardMovimneto(event) {
     const cardWidth = card.offsetWidth;
@@ -11,17 +15,30 @@ function cardMovimneto(event) {
     const posicaoMouseY = event.clientY - centroPaginaY;
     
 
-    const rotateX = (25 * posicaoMouseY / (cardHeight / 2)).toFixed(2);
+    const rotateX = (( + 1 ) * 25 * posicaoMouseY / (cardHeight / 2)).toFixed(2);
 
-    const rotateY = (25 * posicaoMouseX / (cardWidth / 2)).toFixed(2);
+    const rotateY = (( - 1 ) * 25 * posicaoMouseX / (cardWidth / 2)).toFixed(2);
 
-    document.querySelector("h2").innerHTML = rotateX;
-    document.querySelector("h3").innerHTML = rotateY;
+    document.querySelector("#eixoX").innerHTML = "Posição do eixo X "+ rotateX;
+    document.querySelector("#eixoY").innerHTML = "Posição do eixo Y " + rotateY;
 
     card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-   
-   
+}
+
+function cardBack(event) {
+    card.style.transform = `perspective(500px) rotateX(0deg) rotateY(0deg)`;
     
-    
-    
+    cardTransition();
+}
+
+function cardTransition() {
+    clearInterval(card.transitionId);
+    card.style.transition = `transform 400ms`;
+    card.transitionId = setTimeout(() => {
+        card.style.transition = "";
+    }, 400 );
+}
+
+function cardEnter(event) {
+    cardTransition();
 }
